64e7b7037a3f7ea85910e0d775813eca398648f3
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-10-03 19:55:38 -0500

    added carousel

---
+++ ecommerce/src/components/CarouselPrincipal.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
+import React, { useEffect, useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+const CarouselPrincipal = () => {
+  const BASE_URL = "https://ecomerce-master.herokuapp.com/api/v1/";
+
+  const [ProductsInfo, setProductsInfo] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}items`)
+      .then(({ data }) => setProductsInfo(data))
+      .catch((error) => console.log("error calling SWAPI"));
+  }, []);
+
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default CarouselPrincipal;
