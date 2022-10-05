d99c1eaa4a0214f972abfb1d317d7fbc281544da
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-10-05 00:26:07 -0500

    fix carousel

--- ecommerce/src/components/CarouselPrincipal.jsx
+++ ecommerce/src/components/CarouselPrincipal.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 import React, { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
+import "./Carousel.css"
 
 const CarouselPrincipal = () => {
   const BASE_URL = "https://ecomerce-master.herokuapp.com/api/v1/";
@@ -16,45 +17,48 @@ const CarouselPrincipal = () => {
 //   }, []);
 
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-          alt="Second slide"
-        />
+    
+      <Carousel >
+        <Carousel.Item>
+          <img
+            
+            className="d-block w-100"
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+            alt="Second slide"
+          />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-          alt="Third slide"
-        />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+            alt="Third slide"
+          />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    
   );
 };
 
