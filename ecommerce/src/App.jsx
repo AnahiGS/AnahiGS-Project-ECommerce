a36a54af889ee85e3b2b167411998b419e02b63e
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-10-24 19:23:12 -0500

    feat: se modificaron los productos

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -3,14 +3,19 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CarouselPrincipal from "./components/CarouselPrincipal";
 import NavbarComponent from "./components/Layer";
+import Producto from "./components/Producto";
 import Productos from "./components/Productos";
 
 function App() {
+
+
+  
   return (
     <div className="App">
       <NavbarComponent />
       <CarouselPrincipal />
-      <Productos />
+      <Producto />
+      <Productos/>
     </div>
   );
 }
--- ecommerce/src/components/Productos.jsx
+++ ecommerce/src/components/Productos.jsx
@@ -1,43 +1,23 @@
-import React, {useState, useEffect} from "react";
-import { Card, Button, Row } from "react-bootstrap";
-import axios from 'axios';
+import React from 'react'
+import Producto from './Producto'
+
 
 const Productos = () => {
-  const BASE_URL = "https://ecomerce-master.herokuapp.com/api/v1/";
-  const [ProductsArray, setProductsArray] = useState([]);
-  const [ProductsInfo, setProductsInfo] = useState({})
-
-  
-  useEffect(() => {
-
-    axios
-      .get(`${BASE_URL}item`)
-      .then((data) => setProductsArray(data.data))
-      .catch((error) => console.log("error calling API"));
-  }, []);
-
   return (
-    <>
-      {ProductsArray.length===0 ? (<h3>cargando productos</h3>)
-      : (ProductsArray.map((item,index) => (
-        <div key={index}> 
-          <Row>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={item.image} />
-              <Card.Body>
-                <Card.Title>{item.product_name}</Card.Title>
-                <Card.Text>
-                  {item.description}
-                </Card.Text>
-                <Button variant="primary">Agregar al carrito</Button>
-              </Card.Body>
-            </Card>
-          </Row>
-       </div> 
-      ))
-      )}
-    </>
-  );
-};
+    <div className="container">
+        <div className="row">
+            <div className="col-md-4">
+                <Producto/>
+            </div>
+            <div className="col-md-4">
+                <Producto/>
+            </div>
+            <div className="col-md-4">
+                <Producto/>
+            </div>
+        </div>
+    </div>
+  )
+}
 
-export default Productos;
+export default Productos
