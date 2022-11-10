39bc79d104cb88b06b58bcaf647aa50ffac453dc
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-11-10 16:53:13 -0600

    feat: changed sidebar to horizontal bar

--- ecommerce/src/components/Sidebar.jsx
+++ ecommerce/src/components/Sidebar.jsx
@@ -3,30 +3,28 @@ import { Stack , Card, Container, Row} from 'react-bootstrap';
 
 const SidebarSearch = () => {
     return (
-        <>
-        <h3>Categories</h3>
-        <Container fluid className='product-details m-3'>
-            <Row>             
-                <div className='card-stack'>
-                    <Stack direction="horizontal" gap={3}>
-                        <div className="bg-light border"><Card.Title>Health</Card.Title>
-                        </div>
-                        <div className="bg-light border"><Card.Title>Outdoors</Card.Title>
-                        </div>
-                        <div className="bg-light border"><Card.Title>Grocery</Card.Title>
-                        </div>
-                        <div className="bg-light border"><Card.Title>Sports</Card.Title>
-                        </div>
-                        <div className="bg-light border"><Card.Title>Kids</Card.Title>
-                        </div>
-                        <div className="bg-light border"><Card.Title>Jewerly</Card.Title>
-                        </div>    
-                    </Stack>
-                </div>
-            </Row>
-        </Container>
-        
-        </>
+        <> 
+            <h1>Categories</h1>
+            <Row>
+            
+            <Container>
+                <Stack direction="horizontal" gap={3} >
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} ><Card.Title>Health</Card.Title>
+                    </div>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }}><Card.Title>Outdoors</Card.Title>
+                    </div>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }}><Card.Title>Grocery</Card.Title>
+                    </div>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }}><Card.Title>Sports</Card.Title>
+                    </div>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }}><Card.Title>Kids</Card.Title>
+                    </div>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }}><Card.Title>Jewerly</Card.Title>
+                    </div>    
+                </Stack>
+            </Container>
+        </Row>  
+        </> 
     );
 };
 
