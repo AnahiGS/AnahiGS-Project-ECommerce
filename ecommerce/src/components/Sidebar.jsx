9cb22631b34dc9b0d3272d170075933d722f626d
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-11-15 20:51:17 -0600

    trial to add filter to sidebar

--- ecommerce/src/components/Sidebar.jsx
+++ ecommerce/src/components/Sidebar.jsx
@@ -1,14 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Stack , Card, Container, Row} from 'react-bootstrap';
+import { useProductContext } from '../context/ProductContext';
+import Producto from './Producto';
 // import { useProductContext } from '../context/ProductContext';
 
 
 const SidebarSearch = () => {
-    // const context = useProductContext()
+    const context = useProductContext()
+    
 
-    const cardTitle= (event) =>{
+    const filterResult= (catItem) =>{
         // context.setCategories(event.target.name)
-        console.log(event.target)
+        context.productList.filter((item) =>{
+            return item.category === catItem
+        })
     }
     
     return (
@@ -17,22 +22,41 @@ const SidebarSearch = () => {
             <Row>
             
             <Container>
-                <Stack direction="horizontal" gap={3} onClick={cardTitle}>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Health' ><Card.Title name='Health'>Health</Card.Title>
+                <Stack direction="horizontal" gap={3} >
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} onClick={() => filterResult('Health')} ><Card.Title >Health</Card.Title>
                     </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Outdoors'><Card.Title>Outdoors</Card.Title>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} onClick={() => filterResult('Outdoors')}><Card.Title>Outdoors</Card.Title>
                     </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Grocery'><Card.Title>Grocery</Card.Title>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} onClick={() => filterResult('Grocery')}><Card.Title>Grocery</Card.Title>
                     </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Sports'><Card.Title>Sports</Card.Title>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} onClick={() => filterResult('Sports')}><Card.Title>Sports</Card.Title>
                     </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Kids'><Card.Title>Kids</Card.Title>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} onClick={() => filterResult('Kids')}><Card.Title>Kids</Card.Title>
                     </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Jewerly'><Card.Title>Jewerly</Card.Title>
+                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} onClick={() => filterResult('Jewerly')}><Card.Title>Jewerly</Card.Title>
                     </div>    
                 </Stack>
             </Container>
         </Row>  
+        <Row>
+          
+          <div className='col'>
+            <div id='box-products' className='row row-cols-4'>
+                { context.loading
+                  ? <p className='textLoading py-2'>Cargando...</p>
+                  : context.productList.map((item, index) => ((
+                        <div key={index} className='cardP '>
+
+                            <Producto
+                            {...item}/>
+                        </div>
+                      )             
+                    ) 
+                  )
+                }  
+            </div>
+          </div>
+      </Row>
         </> 
     );
 };
