8bd3e78c33be7dc56f75479bb6a68cc7396ac803
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-11-17 00:31:50 -0600

    added swip slider

--- ecommerce/src/components/Sidebar.jsx
+++ ecommerce/src/components/Sidebar.jsx
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
 import { Stack , Card, Container, Row} from 'react-bootstrap';
+import '../styles/index.css'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import {FreeMode} from 'swiper'
+import 'swiper/css'
+import "swiper/css/free-mode"
 // import { useProductContext } from '../context/ProductContext';
 
 
@@ -12,28 +17,58 @@ const SidebarSearch = () => {
     }
     
     return (
-        <> 
-            <h1>Categories</h1>
-            <Row>
-            
-            <Container>
-                <Stack direction="horizontal" gap={3} onClick={cardTitle}>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Health' ><Card.Title name='Health'>Health</Card.Title>
+        <div className="col-12 d-flex align-content-center justify-content-center">
+            <div style={{ width: "80%", marginLeft: "auto", marginRight: "auto" }}>
+                <div className="row">
+                <div className="col-lg-12">
+                    <div className="section-head-style-one">
+                    <h1>Categories</h1>
                     </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Outdoors'><Card.Title>Outdoors</Card.Title>
-                    </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Grocery'><Card.Title>Grocery</Card.Title>
-                    </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Sports'><Card.Title>Sports</Card.Title>
-                    </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Kids'><Card.Title>Kids</Card.Title>
-                    </div>
-                    <div className="bg-light border rounded" style={{ cursor: 'pointer' }} name='Jewerly'><Card.Title>Jewerly</Card.Title>
-                    </div>    
-                </Stack>
-            </Container>
-        </Row>  
-        </> 
+                </div>
+                </div>
+
+                
+                <div className='container py-4 px-4 justify-content-center bg-light'>
+                    <Swiper
+                    FreeMode={true}
+                    grabCursor={true}
+                    modules={[FreeMode]}
+                    className='mySwiper'
+                    slidesPerView={5}
+                    spaceBetween={10}
+                    >
+                        <SwiperSlide>
+                            <h1 style={{ cursor: 'pointer' }} name='Health' >Health
+                            </h1>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <h1 style={{ cursor: 'pointer' }} name='Outdoors'>Outdoors
+                            </h1>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <h1 style={{ cursor: 'pointer' }} name='Grocery'>Grocery
+                        </h1>  
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <h1 style={{ cursor: 'pointer' }} name='Sports'>Sports
+                        </h1>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <h1 style={{ cursor: 'pointer' }} name='Kids'>Kids
+                        </h1>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <h1 style={{ cursor: 'pointer' }} name='Jewerly'>Jewerly
+                        </h1>
+                        </SwiperSlide>
+                        
+                        
+                            
+                    </Swiper>
+                </div>
+                   
+            </div>
+        </div> 
     );
 };
 
